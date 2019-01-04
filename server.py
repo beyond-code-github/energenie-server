d1a819734fe508432d549b3b56d26e5c8d70c7f4
Pete Smith <github@beyond-code.com>
2019-01-04 13:48:34 +0000

    Attempting to handle messages from TRV

--- server.py
+++ server.py
@@ -2,8 +2,16 @@ import paho.mqtt.client as mqtt
 import os
 import atexit
 import energenie
+from energenie.Devices import MIHO013
 
 energenie.init()
+
+
+class Trv(MIHO013):
+    def handle_message(self, payload):
+        result = super(Trv, self).handle_message(payload)
+        print("Something extra")
+        return result
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -12,23 +20,27 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("home/energenie/*/*")
+    client.subscribe("home/#")
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    path = str.split(msg.topic)
-    house_code = path[2]
-    switch_idx = path[3]
+    payload = msg.payload.decode("utf-8")
+    print(msg.topic + " - " + payload)
+
+    path = str.split(msg.topic, "/")
+    house_code = int(path[2], 16)
+    switch_idx = int(path[3])
 
     switch = energenie.Devices.ENER002((house_code, switch_idx))
+    print("house_code: " + str(house_code) + " idx: " + str(switch_idx))
 
-    if msg.payload == 'ON':
+    if payload == "ON":
+        print("Turning switch on")
         switch.turn_on()
     else:
+        print("Turning switch off")
         switch.turn_off()
-
-    print(msg.topic + " - " + str(msg.payload))
 
 
 client = mqtt.Client()
@@ -40,11 +52,8 @@ client.username_pw_set("homeassistant", token)
 
 client.connect("localhost", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+# spare_room_rad
+valve = Trv(8220)
 
 
 def onexit():
@@ -56,3 +65,11 @@ def onexit():
 
 
 atexit.register(onexit)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+while True:
+    if not energenie.loop():
+        client.loop()
